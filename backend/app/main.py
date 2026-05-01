b5def5469496092a5b55178a103b6cead8595ea2
sss135790 <shwetsingh32@gmail.com>
2026-05-01 21:23:25 +0530

    Added a home Route for api check

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,6 +6,11 @@ from app.core.database import Base, engine
 
 app = FastAPI()
 
+
+@app.get("/")
+def root():
+    return {"message": "API is running 🚀"}
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
